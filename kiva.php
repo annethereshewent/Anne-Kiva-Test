15817435cb3e8da8a6cccc42a88d3c5109ddaa7d
Anne Castrillon <anne.castrillon@gmail.com>
2017-09-06 22:02:19 -0700

    Got everything mostly working, just need to do the unit testing

--- kiva.php
+++ kiva.php
@@ -1,20 +1,25 @@
 <?php
 //first we need to do a curl call to the API and parse the json
 
+
+
+
+
+$loan_total = 0;
+//setlocale(LC_MONETARY, 'en_US.UTF-8');
+
+
+
+
 $curl = curl_init();
 
 curl_setopt_array($curl, [
-	CURLOPT_URL 			=> 'http://api.kivaws.org/v1/loans/search.json?status=fundraising&expiringSoon=true',
+	CURLOPT_URL 			=> 'http://api.kivaws.org/v1/loans/search.json?status=fundraising&expiring_soon=true&per_page=50&sort_by=expiration&page=1',
 	CURLOPT_RETURNTRANSFER  => 1,
 ]);
 
 
 $result = json_decode(curl_exec($curl));
-
-
-
-$loan_total = 0;
-setlocale(LC_MONETARY, 'en_US.UTF-8');
 
 ?>
 <head>
@@ -77,30 +82,44 @@ setlocale(LC_MONETARY, 'en_US.UTF-8');
 			font-size: 20px;
 			cursor: hand;
 		}
+		.loading {
+			display: none;
+			margin-left: 250px;
+			margin-top: 20px;
+
+		}
+		h3 {
+			text-align: center;
+			font-family: Cambria;
+			font-style: italic;
+			font-size: 14px;
+		}
 	</style>
 </head>
 <body>
-<h1>Loans Retrieved From Kiva Database</h1>
+<h1>Loans Retrieved From Kiva Database With at least 24 hours Expiration</h1>
+<h3>(Click on a loan's name to see more details on that loan.)</h3>
 <table class="loan-table" colspacing="0">
 	<tr>
 		<th>Name</th>
 		<th>Planned Expiration Date</th>
 		<th>Loan Amount</th>
 	</tr>
-<?php foreach ($result->loans as $loan) {  ?>
-	<tr>
-		<td><a href="#" onclick="showLoan(<?= $loan->id ?>)"><?= $loan->name ?></a></td>
-		<td><?= date('M j Y h:i:s a', strtotime($loan->planned_expiration_date)) ?></td>
-		<td><?= money_format('%.2n', $loan->loan_amount) ?></td>
+	<?php foreach ($result->loans as $loan) {  ?>
+		<tr>
+			<td><a href="#" onclick="showLoan(<?= $loan->id ?>)"><?= $loan->name ?></a></td>
+			<td><?= date('F j Y', strtotime($loan->planned_expiration_date)) ?></td>
+			<td>$<?= money_format('%.2n', $loan->loan_amount) ?></td>
+		</tr>
+		<?php $loan_total += $loan->loan_amount; ?>
+	<?php } ?>
+	<tr class="total-row">
+		<td colspan="2"><b>Total:</b></td>
+		<td id="loan-total">$<?= money_format( '%.2n', $loan_total) ?></td>
 	</tr>
-	<?php $loan_total += $loan->loan_amount; ?>
-<?php } ?>
-<tr>
-	<td colspan="2"><b>Total:</b></td>
-	<td><?= money_format( '%.2n', $loan_total) ?></td>
-
 </table>
 
+<img src="loading.gif" class="loading" width="20" height="20">
 
 <div class="modal">
 	<div class="modal-content">
@@ -118,19 +137,82 @@ setlocale(LC_MONETARY, 'en_US.UTF-8');
 <script src="jquery-3.2.1.min.js"></script>
 <script>
 	var loans;
+	var current_page=1;
+	var loading = false;
+
 	$(function() {
 		//this way we can easily iterate through the loans if need be
 		loans = <?= json_encode($result->loans) ?>
 
 		$('.modal').click(function (event) {
-		   if(!$(event.target).closest('.modal-content').length && !$(event.target).is('.modal-content')) {
-		     $(".modal").hide();
-		   }     
+		   	if (!$(event.target).closest('.modal-content').length && !$(event.target).is('.modal-content')) {
+   				$(".modal").hide();
+		   	}     
 		});
 
 		$('.close-button').click(function() {
 			$(".modal").hide();
 		})
+		$(window).scroll(function() {
+			if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+				//need to do an ajax call to retrieve the next set of documents
+				if (!loading) {
+					loading = true;
+					$('.loading').show();
+					$.get(
+						'http://api.kivaws.org/v1/loans/search.json?status=fundraising&expiring_soon=true&per_page=50&sort_by=expiration&page=' + (current_page+1),
+						function(data) {
+							console.log(data);
+
+							var total = parseInt($("#loan-total").text().substring(1));
+							console.log(total);
+
+							var new_loans = data.loans;
+
+
+							//this will get the next day so we can compare the results and filter them out
+							tomorrow_date = new Date();
+							tomorrow_date.setDate(tomorrow_date.getDate() + 1);
+
+
+							//don't do anything if the first result already has a time greater than tomorrow 
+							if (new Date(new_loans[0].planned_expiration_date).getTime() > tomorrow_date.getTime()) {
+								$('.loading').hide();
+								return;
+							}
+
+
+							//remove the total row, it's invalid 
+							$(".total-row").remove();
+
+							for (var i = 0; i < new_loans.length; i++) {
+								if (new Date(new_loans[i].planned_expiration_date).getTime() <= tomorrow_date.getTime()) {
+									//if the expiration date is within one day, then append it to the table
+									$(".loan-table").append("<tr><td><a href='#' onclick='showLoan(" + new_loans[i].id + ")'>" + new_loans[i].name + "</a></td><td>" + formatDate(new Date(new_loans[i].planned_expiration_date)) + "</td><td>$" + new_loans[i].loan_amount.toFixed(2) + "</td>");
+									total += parseInt(new_loans[i].loan_amount); 
+								}
+								else {
+									//remove this element from new loans since its date is later than 24 hours
+									new_loans.splice(i,1);
+								}
+							}
+
+							//append the new_loans to the loans array so that we can iterate through them
+							loans.push.apply(loans, new_loans);
+
+							$(".loan-table").append('<tr class="total-row"><td colspan="2"><b>Total:</b> </td><td id="loan-total">$' + total.toFixed(2)  + '</td></tr>')
+
+
+							$('.loading').hide();
+							loading = false;
+							current_page++;
+						},
+						'json'
+					);
+				}
+			} 
+		})
+
 	});
 
 	function showLoan(loan_id) {
@@ -144,7 +226,7 @@ setlocale(LC_MONETARY, 'en_US.UTF-8');
 			$("#expiration_date").text(loan.planned_expiration_date);
 			$("#funded_amount").text("$" + loan.funded_amount.toFixed(2));
 
-			$("#amount_remaining").text("$" + (loan.loan_amount - loan.funded_amount).toFixed(2	));
+			$("#amount_remaining").text("$" + (loan.loan_amount - loan.funded_amount).toFixed(2));
 
 			$(".modal").show();
 		}
@@ -153,6 +235,24 @@ setlocale(LC_MONETARY, 'en_US.UTF-8');
 		}
 	}
 
+	function formatDate(date) {
+		var months = [
+			'January',
+			'February',
+			'March',
+			'April',
+			'May',
+			'June',
+			'July',
+			'August',
+			'September',
+			'October',
+			'November',
+			'December'
+		];
+
+		return months[date.getMonth()] + " " + date.getDate() + " " + date.getFullYear();
+	}
 	function getLoan(loan_id) {
 		for (var i = 0; i < loans.length; i++) {
 			if (loans[i].id == loan_id) {
